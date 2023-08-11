fe29420166bf82643aa292ae3714f732a02b1cf9
Võ Quang Linh <21dh111007@st.huflit.edu.vn>
2023-08-11 21:04:41 +0700

    Linh push 11/8

---
+++ Controllers/ChartController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo_CNPM.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Chart()
+        {
+            return View();
+        }
+    }
+}
